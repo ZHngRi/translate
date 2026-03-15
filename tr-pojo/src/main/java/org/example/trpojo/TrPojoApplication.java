215c95d0621232c61d896a4c8e0876d684c617e8
zhngri <424512059@qq.com>
2026-03-16 07:04:11 +0800

    给pojo加上TrPojoApplication来防止mvn clean package对TrPojoApplicationTest的报错，看不懂问gpt

---
+++ tr-pojo/src/main/java/org/example/trpojo/TrPojoApplication.java
@@ -0,0 +1,13 @@
+package org.example.trpojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrPojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrPojoApplication.class, args);
+    }
+
+}
